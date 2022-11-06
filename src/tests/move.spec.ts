679b4fb8ced27dd68477cd48f4ecaa1764ed6cf8
Joed <parinjoed@gmail.com>
2022-11-06 17:21:36 +0800

    test: add more unit test

--- src/tests/move.spec.ts
+++ src/tests/move.spec.ts
@@ -49,4 +49,52 @@ describe("Command MOVE", function () {
     expect(y).toBe(1);
     expect(x).toBe(0);
   });
+
+  it("should fail moving outside the table east x axis", function () {
+    const robots = [{ id: "genesis", x: 4, y: 1, f: FACING.EAST }];
+    const [{ x, y }] = makeMove({
+      robotId: "genesis",
+      robots,
+      tableXUnit: DEFAULT_TABLE_X_UNIT,
+      tableYUnit: DEFAULT_TABLE_Y_UNIT,
+    });
+    expect(y).toBe(1);
+    expect(x).toBe(4); // expected not to increment since the robot will fall to the table
+  });
+
+  it("should fail moving outside the table west x axis", function () {
+    const robots = [{ id: "genesis", x: 0, y: 1, f: FACING.WEST }];
+    const [{ x, y }] = makeMove({
+      robotId: "genesis",
+      robots,
+      tableXUnit: DEFAULT_TABLE_X_UNIT,
+      tableYUnit: DEFAULT_TABLE_Y_UNIT,
+    });
+    expect(y).toBe(1);
+    expect(x).toBe(0); // expected not to decrement since the robot will fall to the table
+  });
+
+  it("should fail moving outside the table north y axis", function () {
+    const robots = [{ id: "genesis", x: 0, y: 4, f: FACING.NORTH }];
+    const [{ x, y }] = makeMove({
+      robotId: "genesis",
+      robots,
+      tableXUnit: DEFAULT_TABLE_X_UNIT,
+      tableYUnit: DEFAULT_TABLE_Y_UNIT,
+    });
+    expect(y).toBe(4); // expected not to increment since the robot will fall to the table
+    expect(x).toBe(0);
+  });
+
+  it("should fail moving outside the table south y axis", function () {
+    const robots = [{ id: "genesis", x: 0, y: 0, f: FACING.SOUTH }];
+    const [{ x, y }] = makeMove({
+      robotId: "genesis",
+      robots,
+      tableXUnit: DEFAULT_TABLE_X_UNIT,
+      tableYUnit: DEFAULT_TABLE_Y_UNIT,
+    });
+    expect(y).toBe(0); // expected not to increment since the robot will fall to the table
+    expect(x).toBe(0);
+  });
 });
